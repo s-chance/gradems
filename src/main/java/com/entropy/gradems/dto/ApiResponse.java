1b48773752bb7b6338dce63fa0c2106dfd9aaa1c
s-chance <entropy.tree@outlook.com>
2024-02-09 20:40:02 +0800

    feat(ApiResponse.java): add general response class

---
+++ src/main/java/com/entropy/gradems/dto/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.entropy.gradems.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private int code;
+    private T data;
+}
