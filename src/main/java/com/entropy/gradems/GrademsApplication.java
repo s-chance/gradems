35a9dfdeaab5adf46b781f3e9a0e644a200e0a91
s-chance <entropy.tree@outlook.com>
2024-02-09 21:15:54 +0800

    refactor: remove unnecessary annotation

--- src/main/java/com/entropy/gradems/GrademsApplication.java
+++ src/main/java/com/entropy/gradems/GrademsApplication.java
@@ -1,11 +1,9 @@
 package com.entropy.gradems;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.entropy.gradems.mapper")
 public class GrademsApplication {
 
     public static void main(String[] args) {
